33602982e45ea299115408fc78af33ba98b9b6e7
Italo Queiroz <batutista14@gmail.com>
2020-10-08 10:25:52 -0300

    filtros

--- Dia 3/lista-compras.js
+++ Dia 3/lista-compras.js
@@ -4,25 +4,36 @@
     // MODULO
     angular.module('listaTarefasApp', []);
 
+    // FILTROS
+    // criando um filtro
+    angular.module('listaTarefasApp')
+        .filter('capitalize', function(){
+            return function(input) {return input.charAt(0).toUpperCase() + input.substring(1); }
+        });
+
     // CONTROLLER
     angular.module('listaTarefasApp')
         .controller('ListaTarefasController', listaTarefasController);
 
-    listaTarefasController.$inject = ['$rootScope'];
+    // nomeDoFiltro + Filter => só chama o filtro específico. Ex: limitToFilter
+    listaTarefasController.$inject = ['$rootScope', 'limitToFilter'];
 
-    function listaTarefasController($rootScope) {
+    function listaTarefasController(root, limitTo) {
         var vm = this;
         /* ***************    INIT VARIÁVEIS    *********************************** */
         vm.logado = false;
         vm.validar = false;
         vm.register = false;
 
-        $rootScope.listaMensagens = [
+        root.listaMensagens = [
             { id: 1, text: 'Mensagem aqui', tipo: 'danger'},
             { id: 2, text: 'Mensagem aqui', tipo: 'info'},
-            { id: 2, text: 'Mensagem aqui', tipo: 'success'},
+            { id: 3, text: 'Mensagem aqui', tipo: 'success'},
             { id: 4, text: 'Mensagem aqui', tipo: 'warning'}
         ]
+
+        vm.teste = 'teste'
+        vm.teste2 = limitTo(vm.teste, 3);
 
         /* ***************    FUNÇÕES EXECUTADAS NA VIEW (HMTL)    **************** */
 
